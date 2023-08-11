f8496b1130066e0af5931c465c73fbaddf16984e
andrea <andreatv12@gmail.com>
2023-08-11 08:21:26 +0100

    container gets called by frontend

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,20 +1,22 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/test")
+@CrossOrigin
 public class TestController {
 
     // curl -X GET http://localhost:8080/test/
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/")
     @ResponseBody
-    public String getTest(){
-        return "hi";
+    public String getTest() throws Exception {
+        return "aaaaaaahi";
     }
 
 }
