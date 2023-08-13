1c9b5a50a1c6139cc870118a7fbdbfbd82f040e8
andrea <andreatv12@gmail.com>
2023-08-13 09:46:24 +0100

    kubernetes allow cors

--- src/main/java/com/example/demo/CorsConfig.java
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Update with your frontend URL
+                .allowedOrigins("http://localhost:3000", "http://localhost:30000") // Update with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
