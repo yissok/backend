62408b9dc161439dd06f0b8015f3a7a59a44cadc
yissok <andreatv12@gmail.com>
2024-06-19 12:11:18 +0100

    local db

--- pre-integration.sh
+++ pre-integration.sh
@@ -1,2 +1,7 @@
 #docker compose start mysql container
 #runmysql command to start mysql at port 3306 localhost
+MYSQL_PASSWORD=mysqlr00t
+DB_NAME=andrea_test_db
+PWD=$(pwd)
+MYSQL_PASSWORD=$MYSQL_PASSWORD docker-compose up --build -d --remove-orphans --force-recreate
+docker exec -it mysql8 mysql -uroot -p$MYSQL_PASSWORD -h localhost --port 3306 --protocol=TCP -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
